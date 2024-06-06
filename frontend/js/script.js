0f14f548f4ab2d2891e5065b290ef27a64b3e690
mayk-dev <62581134+mayk-dev@users.noreply.github.com>
2024-06-06 08:31:56 -0300

    at

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("https://maykdev-chat.onrender.com")
+    websocket = new WebSocket("https://landingpagepessoal-1.onrender.com")
     websocket.onmessage = processMessage
 }
 
